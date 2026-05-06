6efd52ce764b4b9c0b046e78ff60838c51806e27
SilverMum <silvers.vibrant.ventures@outlook.com>
2026-05-05 22:52:29 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -17,37 +17,19 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
     /* -----------------------------------------------------
-       2. EVENTS DROPDOWN (TOP LEVEL)
+       2. UNIVERSAL MOBILE DROPDOWN TOGGLE (EVENTS + GROUPS)
        ----------------------------------------------------- */
-    const eventsToggle = document.querySelector('.nav-links > li.dropdown:nth-of-type(1) > a');
-    const eventsMenu = document.querySelector('.nav-links > li.dropdown:nth-of-type(1) > .dropdown-menu');
-
-    if (eventsToggle && eventsMenu) {
-        eventsToggle.addEventListener('click', function (e) {
+    document.querySelectorAll('.dropdown > a').forEach(link => {
+        link.addEventListener('click', function (e) {
             if (window.innerWidth <= 900) {
                 e.preventDefault();
-                eventsMenu.classList.toggle('open');
+                this.parentElement.classList.toggle('open');
             }
         });
-    }
+    });
 
     /* -----------------------------------------------------
-       3. GROUPS DROPDOWN (TOP LEVEL)
-       ----------------------------------------------------- */
-    const groupsToggle = document.querySelector('.nav-links > li.dropdown:nth-of-type(2) > a');
-    const groupsMenu = document.querySelector('.nav-links > li.dropdown:nth-of-type(2) > .dropdown-menu');
-
-    if (groupsToggle && groupsMenu) {
-        groupsToggle.addEventListener('click', function (e) {
-            if (window.innerWidth <= 900) {
-                e.preventDefault();
-                groupsMenu.classList.toggle('open');
-            }
-        });
-    }
-
-    /* -----------------------------------------------------
-       4. MEMBERS SUBMENU (RIGHT-OPENING)
+       3. MEMBERS SUBMENU (RIGHT-OPENING)
        ----------------------------------------------------- */
     const membersToggle = document.querySelector('.dropdown-sub > .submenu-link');
     const membersMenu = document.querySelector('.dropdown-sub > .submenu-right');
