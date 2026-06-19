592aecc2a8f52ef03ab7e9b46467ab9796e0795b
SilverMum <silvers.vibrant.ventures@outlook.com>
2026-06-18 22:57:37 -0400

    Delete scripts/load-nav.js

--- scripts/load-nav.js
+++
@@ -1,8 +0,0 @@
-fetch("components/nav.html")
-  .then(response => response.text())
-  .then(html => {
-      document.getElementById("nav-placeholder").innerHTML = html;
-  })
-  .catch(err => {
-      console.error("Navigation failed to load:", err);
-  });
